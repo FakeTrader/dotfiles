e131139c996186e26e7e8140af5e61225985588e
W Han <9635792+FakeTrader@users.noreply.github.com>
2023-03-24 21:50:32 +0800

    gnu exec

--- bash/.bashrc.d/env.sh
+++ bash/.bashrc.d/env.sh
@@ -7,3 +7,11 @@ export HOMEBREW_CLEANUP_MAX_AGE_DAYS=3
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_ENV_HINTS=1
+# GNU exec
+# export PATH="/usr/local/opt/sqlite/bin:$PATH"
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/gettext/bin:$PATH"
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
