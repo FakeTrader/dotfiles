2850acbb9a0cb7ae24a9121319e3543a5a297eef
W Han <9635792+FakeTrader@users.noreply.github.com>
2023-03-24 21:52:03 +0800

    macos bash

--- bash/.bashrc.d/cfg.sh
+++ bash/.bashrc.d/cfg.sh
@@ -9,3 +9,10 @@ export CLICOLOR=yes
 
 # bash completion (Homebrew)
 [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+
+# bash on macOS 10.15+
+# a) silence warning
+export BASH_SILENCE_DEPRECATION_WARNING=1
+# b) Install latest bash with homebrew
+# go to System Preferences > Ussers & Groups, unlock, right click
+# on user, Advanced Options, set Login shell to /usr/local/bin/bash
