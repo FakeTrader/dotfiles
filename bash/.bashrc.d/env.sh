12c3160f10d82153116935262398d6458d4a700b
W Han <9635792+FakeTrader@users.noreply.github.com>
2023-03-24 21:51:21 +0800

    Update env.sh

--- bash/.bashrc.d/env.sh
+++ bash/.bashrc.d/env.sh
@@ -1,3 +1,9 @@
+# Defining a Bash Variable With or Without ‘export’
+# https://www.baeldung.com/linux/bash-variables-export
+
+## Executable
+export PATH="$HOME/Applications/bin:$PATH"
+
 ## Homebrew
 # export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
 # export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
@@ -7,6 +13,7 @@ export HOMEBREW_CLEANUP_MAX_AGE_DAYS=3
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_ENV_HINTS=1
+
 # GNU exec
 # export PATH="/usr/local/opt/sqlite/bin:$PATH"
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
@@ -25,6 +32,16 @@ export PATH="/usr/local/opt/sqlite/bin:$PATH"
 export OSFONTDIR=$HOME/Library/Fonts//:/Library/Fonts//:/System/Library/Fonts//
 export CHKTEXRC=$HOME/dotfiles/rc
 export LATEXMKRCSYS=$HOME/dotfiles/rc/latexmkrc
+
+## Compilers
+# export LDFLAGS="-L/usr/local/opt/gettext/lib"
+# export CPPFLAGS="-I/usr/local/opt/gettext/include"
+
+# Algorithm 4
+# export CLASSPATH="$CLASSPATH:~/Documents/algs4/algs4.jar"
+
+# CodeExpander will automaticly add path to ~/.profile
+# export PATH=$PATH:/Applications/CodeExpander.app/Contents/Resources/app/nodes:/Users/whan/.once-gui/installation/node_modules/.bin:/Applications/CodeExpander.app/Contents/Resources/app/node_modules/.bin
 
 ## FPGA
 ## toolchain
