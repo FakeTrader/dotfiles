16dc612942c0fee1a7d9118b8a4fd31e3adb92d3
W Han <9635792+FakeTrader@users.noreply.github.com>
2023-03-24 21:51:46 +0800

    use export

--- bash/.bashrc.d/cfg.sh
+++ bash/.bashrc.d/cfg.sh
@@ -1,5 +1,6 @@
-# Color
-color_prompt=yes
-CLICOLOR=yes
+# Colour prompt
+export color_prompt=yes
+export CLICOLOR=yes
+
 # bash completion (Homebrew)
 [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
