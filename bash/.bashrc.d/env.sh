ed21638f5a0118ff376fc816d111b275536df92e
W Han <9635792+FakeTrader@users.noreply.github.com>
2023-03-24 21:50:15 +0800

    Homebrew

--- bash/.bashrc.d/env.sh
+++ bash/.bashrc.d/env.sh
@@ -1,2 +1,9 @@
+## Homebrew
+# export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
+# export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 # Homebrew
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+# PATH="$(brew --prefix)/path/to/bin:$PATH"
+export HOMEBREW_CLEANUP_MAX_AGE_DAYS=3
+export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_ENV_HINTS=1
