f29b0e7b42df5746871bc9be993015b9825b9462
W Han <9635792+FakeTrader@users.noreply.github.com>
2023-10-07 10:59:27 +0800

    Update no_proxy

--- bash/.bashrc.d/proxy.bash
+++ bash/.bashrc.d/proxy.bash
@@ -1,7 +1,8 @@
 ## Proxy
+# https://about.gitlab.com/blog/2021/01/27/we-need-to-talk-no-proxy/
 export http_proxy="http://127.0.0.1:7890/"
 export https_proxy=$http_proxy
 export HTTP_PROXY=$http_proxy
 export HTTPS_PROXY=$http_proxy
 export ALL_PROXY=$http_proxy
-export no_proxy="127.0.0.1, localhost"
+export no_proxy="127.0.0.1, routerlogin.net, router.asus.com, lan, localdomain, example, invalid, localhost, test, local"
