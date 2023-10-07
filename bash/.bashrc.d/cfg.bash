941aa3c8873c02304dfe28adb76adcada074efe9
W Han <9635792+FakeTrader@users.noreply.github.com>
2023-10-07 11:05:44 +0800

    Update macOS

--- bash/.bashrc.d/cfg.bash
+++ bash/.bashrc.d/cfg.bash
@@ -7,12 +7,3 @@ export LANGUAGE=en_US.UTF-8
 export color_prompt=yes
 export CLICOLOR=yes
 
-# bash completion (Homebrew)
-[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
-
-# bash on macOS 10.15+
-# a) silence warning
-export BASH_SILENCE_DEPRECATION_WARNING=1
-# b) Install latest bash with homebrew
-# go to System Preferences > Ussers & Groups, unlock, right click
-# on user, Advanced Options, set Login shell to /usr/local/bin/bash
--- bash/.bashrc.d/macos.bash
+++ bash/.bashrc.d/macos.bash
@@ -1,2 +1,10 @@
+# bash on macOS 10.15+
+# a) silence warning
+export BASH_SILENCE_DEPRECATION_WARNING=1
+# b) Install latest bash with homebrew
+# go to System Preferences > Ussers & Groups, unlock, right click
+# on user, Advanced Options, set Login shell to /usr/local/bin/bash
+
+
 alias dsclean='find . -name ".DS_Store" -type f -delete'
 alias flush='dscahceutil -flushcache'
