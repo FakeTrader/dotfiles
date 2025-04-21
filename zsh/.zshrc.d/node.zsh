bbcd241ec666c5ba38bcd666b99fa7d8fa71ed35
FakeTrader <9635792+FakeTrader@users.noreply.github.com>
2025-04-21 14:03:31 +0800

    feat: package management

--- zsh/.zshrc.d/node.zsh
+++ zsh/.zshrc.d/node.zsh
@@ -1,6 +1,10 @@
+# NVM
 # export NVM_DIR="$HOME/.nvm"
 export NVM_DIR="/opt/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 # export NVM_DIR="/opt/homebrew/opt/nvm" # Homebrew Apple
 # export NVM_DIR="/usr/local/opt/nvm" # Homebrew Intel
+
+# PNPM
+alias pn=pnpm
--- zsh/.zshrc.d/python.zsh
+++ zsh/.zshrc.d/python.zsh
@@ -1,3 +1,8 @@
+# Conda
 # https://www.anaconda.com/blog/conda-configuration-engine-power-users
 export CONDA_AUTO_ACTIVATE_BASE=false
 PATH="/opt/conda/bin:$PATH"
+
+# uv
+export UV_PYTHON_INSTALL_DIR="/opt/uv/python"
+export UV_TOOL_DIR="/opt/uv/tool"
