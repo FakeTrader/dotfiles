94c888660dff9e6aa53675a5ef1b41d8161ce7a7
FakeTrader <9635792+FakeTrader@users.noreply.github.com>
2025-09-18 10:46:36 +0800

    JDK 25

--- zsh/.zshrc.d/java.zsh
+++ zsh/.zshrc.d/java.zsh
@@ -19,7 +19,7 @@ alias jdk8="jdk 1.8"
 alias jdk11="jdk 11"
 alias jdk17="jdk 17"
 alias jdk21="jdk 21"
-# alias jdk25="jdk 25"
+alias jdk25="jdk 25"
 
 alias graal21="graal 21"
 export GRAALVM_HOME=/Library/Java/JavaVirtualMachines/graalvm-21.jdk/Contents/Home
@@ -27,7 +27,8 @@ export GRAALVM_HOME=/Library/Java/JavaVirtualMachines/graalvm-21.jdk/Contents/Ho
 # export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 # export JAVA_HOME=`/usr/libexec/java_home -v 17`
 # export JAVA_HOME=`/usr/libexec/java_home -v 21`
-set_sdk "jdk" 21
+# export JAVA_HOME=`/usr/libexec/java_home -v 25`
+set_sdk "jdk" 25
 
 ## Tomcat
 export PATH="$HOMEBREW_PREFIX/opt/tomcat@9/bin:$PATH"
@@ -47,11 +48,13 @@ alias tomcatoff='$CATALINA_HOME/bin/shutdown.sh'
 # export JAVA_8_HOME=/Library/Java/JavaVirtualMachines/jdk-1.8.jdk/Contents/Home
 # export JAVA_17_HOME=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home
 # export JAVA_21_HOME=/Library/Java/JavaVirtualMachines/jdk-21.jdk/Contents/Home
+# export JAVA_25_HOME=/Library/Java/JavaVirtualMachines/jdk-25.jdk/Contents/Home
 
 # export JAVA_8_HOME=`/usr/libexec/java_home -v 1.8`
 # export JAVA_11_HOME=`/usr/libexec/java_home -v 11`
 # export JAVA_17_HOME=`/usr/libexec/java_home -v 17`
 # export JAVA_21_HOME=`/usr/libexec/java_home -v 21`
+# export JAVA_25_HOME=`/usr/libexec/java_home -v 25`
 
 # export JAVA_HOME=$JAVA_8_HOME
 
@@ -59,6 +62,7 @@ alias tomcatoff='$CATALINA_HOME/bin/shutdown.sh'
 # alias jdk11="export JAVA_HOME=$JAVA_11_HOME"
 # alias jdk17="export JAVA_HOME=$JAVA_17_HOME"
 # alias jdk21="export JAVA_HOME=$JAVA_21_HOME"
+# alias jdk25="export JAVA_HOME=$JAVA_25_HOME"
 
 # Homebrew
 # OpenJDK
@@ -67,3 +71,4 @@ alias tomcatoff='$CATALINA_HOME/bin/shutdown.sh'
 # sudo ln -sfn $(brew --prefix)/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
 # sudo ln -sfn $(brew --prefix)/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
 # sudo ln -sfn $(brew --prefix)/opt/openjdk@21/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-21.jdk
+# sudo ln -sfn $(brew --prefix)/opt/openjdk@25/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-25.jdk
